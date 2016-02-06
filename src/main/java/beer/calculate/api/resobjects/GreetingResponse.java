49bf15aaaf0633b7e61025a933be675a5e151716
Evan Duncan <evan.e.duncan@gmail.com>
2016-02-05 23:21:28 -0600

    Initial Commit

---
+++ src/main/java/beer/calculate/api/resobjects/GreetingResponse.java
@@ -0,0 +1,20 @@
+package beer.calculate.api.resobjects;
+
+public class GreetingResponse {
+
+	private final long id;
+	private final String content;
+
+	public GreetingResponse(long id, String content) {
+		this.id = id;
+		this.content = content;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+}
